d908361b2601edbd547a579064a3ecb9b031982c
AlexandraB123 <alexandrabenson123@gmail.com>
2022-11-14 14:24:47 +0000

    Remove unnecessary error handling

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ app.get("/api/topics", getTopics);
 
 //error handling
 app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  } else next(err);
-});
-
-app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "Bad Request" });
-  } else next(err);
-});
-
-app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).send({ msg: "Internal Server Error" });
 });
