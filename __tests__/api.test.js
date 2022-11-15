b00df47fb754c9e7e7617ef68412cd23cc2398ad
AlexandraB123 <alexandrabenson123@gmail.com>
2022-11-15 17:22:38 +0000

    complete GET_api_users including tests

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -304,3 +304,22 @@ describe("/api/articles/:article_id/comments", () => {
       });
   });
 });
+
+describe("/api/users", () => {
+  test("GET:200. Sends an array of users to the client", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.users).toEqual(expect.any(Array));
+        expect(body.users.length).toBeGreaterThan(0);
+        body.users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
